f84bdf6c0cbd179ee033ead3c7b29216450f6260
Akshay Mehra <akshay.m.1711@gmail.com>
2014-11-16 13:34:00 -0500

    Added tesseract function wrapper

--- src/imgtext.py
+++ src/imgtext.py
@@ -1,1 +1,29 @@
-#placeholder
+# placeholder for comments/info
+
+# Function to start OCR subprocess and return recognized text
+import os
+import tempfile
+import subprocess
+
+def ocr (imgpath):
+	tempout = tempfile.NamedTemporaryFile()
+
+	try:
+		rcode = subprocess.call(["tesseract", imgpath, tempout])
+		print rcode 
+
+		if rcode != 0:
+			raise Exception
+	except:
+		print "OCR failed"
+
+	with open(tempout.name + ".txt", "r") as text:
+		ocrdata = text.read()
+
+	os.remove(tempout.name + ".txt")
+	os.remove(tempout.name)
+
+	return ocrdata
+
+
+
